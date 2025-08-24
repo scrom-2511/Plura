9472c53ca87aa8364d3068dcbe92860ed09d1f2e
Scrom <formain337@gmail.com>
2025-08-24 16:20:22 +0530

    checkpoint

--- app/api/auth/signin/route.ts
+++ app/api/auth/signin/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import { prisma } from "@/app/api/lib/prisma";
 
 export const POST = async (req: Request) => {
   try {
--- app/api/lib/prisma.ts
+++ app/api/lib/prisma.ts
@@ -3,11 +3,7 @@ import { PrismaClient } from "@prisma/client";
 // Prevent multiple PrismaClient instances in dev
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query"], // optional: logs SQL queries in dev
-  });
+export const prisma = globalForPrisma.prisma || new PrismaClient({});
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
