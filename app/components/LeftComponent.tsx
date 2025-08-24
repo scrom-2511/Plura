6ea667abd9dd9043324b2a6f2fdb9268c521a92c
Scrom <formain337@gmail.com>
2025-08-24 17:18:41 +0530

    fix alignment of chat history chatname

--- app/components/LeftComponent.tsx
+++ app/components/LeftComponent.tsx
@@ -12,7 +12,7 @@ const LeftComponent = () => {
           <input type="text" className="bg-primary w-full rounded-xl h-12 text-[13px] focus:outline-0 p-5" placeholder="Search..." />
         </div>
         <div className="w-full h-full flex flex-col items-center gap-5 overflow-auto">
-          <div className="bg-primary rounded-xl w-full min-h-12 h-12 text-sm font-bold flex justify-center items-center hover:cursor-pointer">
+          <div className="bg-primary rounded-xl w-full min-h-12 text-[12px] flex items-center hover:cursor-pointer p-5">
             <h3>CHAT!</h3>
             <p>hi</p>
           </div>
