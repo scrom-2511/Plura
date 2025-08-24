a59e66243781169d4ec27017fc170c63300dcb81
Scrom <formain337@gmail.com>
2025-08-24 17:12:07 +0530

    create a left component for chat history and chat search and new chat

---
+++ app/components/LeftComponent.tsx
@@ -0,0 +1,25 @@
+"use client"
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const LeftComponent = () => {
+    const router = useRouter()
+  return (
+    <>
+      <div className="w-full grid grid-rows-[150px_auto] p-5 py-10 overflow-hidden">
+        <div className="w-full flex flex-col items-center gap-5">
+          <button className="bg-primary rounded-xl w-full h-12 text-sm font-bold hover:cursor-pointer" onClick={()=> router.push("/chat/newChat")}>NEW CHAT</button>
+          <input type="text" className="bg-primary w-full rounded-xl h-12 text-[13px] focus:outline-0 p-5" placeholder="Search..." />
+        </div>
+        <div className="w-full h-full flex flex-col items-center gap-5 overflow-auto">
+          <div className="bg-primary rounded-xl w-full min-h-12 h-12 text-sm font-bold flex justify-center items-center hover:cursor-pointer">
+            <h3>CHAT!</h3>
+            <p>hi</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LeftComponent;
