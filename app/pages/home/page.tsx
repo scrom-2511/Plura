86a8261d779d4d56d8997d05bc93672e66bf103a
Scrom <formain337@gmail.com>
2025-08-24 16:19:07 +0530

    remove unnecesarry code

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -1,17 +1,38 @@
 import Chatcomponent from "@/app/components/Chatcomponent";
-import React, { useState } from "react";
+import PromptBox from "@/app/components/PromptBox";
 
 const Home = () => {
-  
-
   return (
     <div className="h-screen w-screen">
       <div className="h-full w-full grid grid-cols-[15%_85%]">
-        <div className=""></div>
+        <div className="w-full grid grid-rows-[150px_auto] p-5 py-10 overflow-hidden">
+          <div className="w-full flex flex-col items-center gap-5">
+            <button className="bg-primary rounded-xl w-full h-12 text-sm font-bold">NEW CHAT</button>
+            <input type="text" className="bg-primary w-full rounded-xl h-12 text-[13px] focus:outline-0 p-5" placeholder="Search..." />
+          </div>
+          <div className="w-full flex flex-col items-center gap-5 overflow-auto">
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+            <div className="bg-primary rounded-xl w-full min-h-12 text-sm font-bold">CHAT1</div>
+          </div>
+        </div>
         <div className="grid grid-cols-4 grid-rows-[auto_100px] gap-3 p-10 max-h-full h-full overflow-hidden">
-          
           <Chatcomponent/>
-          
         </div>
       </div>
     </div>
@@ -20,36 +41,4 @@ const Home = () => {
 
 export default Home;
 
-const NoChatComponent = () => {
-  return (
-    <>
-      <div className="bg-primary rounded-2xl h-40 self-center text-center">CHATGPT</div>
-      <div className="bg-primary rounded-2xl h-40 self-center text-center">DEEPSEEK</div>
-      <div className="bg-primary rounded-2xl h-40 self-center text-center">MISTRAL</div>
-      <div className="bg-primary rounded-2xl h-40 self-center text-center">LLAMA</div>
-    </>
-  );
-};
 
-// {/* <h1 className="text-center text-sm font-bold mb-2">CHATGPT</h1>
-//   <div>
-//                 <div>
-//                   <h6 className="font-bold">User</h6>
-//                   <p>a para</p>
-//                 </div>
-//                 <div>
-//                   <h6 className="font-bold">Model</h6>
-//                   <p>this is a long paragraph i wrote to test the ui paragraph i wrote to test the ui</p>
-//                 </div>
-//               </div>
-
-//               <div>
-//                 <div>
-//                   <h6 className="font-bold">User</h6>
-//                   <p>a para</p>
-//                 </div>
-//                 <div>
-//                   <h6 className="font-bold">Model</h6>
-//                   <p>this is a long paragraph i wrote to test the ui paragraph i wrote to test the ui</p>
-//                 </div>
-//               </div> */}
