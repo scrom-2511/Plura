3ec270a415496bd7f33e7b1a36d74aa232967b89
Scrom <formain337@gmail.com>
2025-08-24 16:16:41 +0530

    create record for models

--- types/types.ts
+++ types/types.ts
@@ -1,12 +1,20 @@
+import { Conversation } from "@prisma/client";
+
 export enum ModelTypes {
-    GPT = "openai/gpt-4o",
-    DEEPSEEK = "z-ai/glm-4.5-air:free",
-    MISTRAL = "mistralai/mistral-small-3.2-24b-instruct:free",
-    QWEN = "nvidia/llama-3.1-nemotron-ultra-253b-v1:free"
+  GPT = "openai/gpt-4o",
+  DEEPSEEK = "z-ai/glm-4.5-air:free",
+  MISTRAL = "mistralai/mistral-small-3.2-24b-instruct:free",
+  QWEN = "moonshotai/kimi-k2:free",
 }
 
+export const modelFieldMap: Record<ModelTypes, keyof Conversation> = {
+    [ModelTypes.GPT]: "gpt",
+    [ModelTypes.DEEPSEEK]: "deepseek",
+    [ModelTypes.MISTRAL]: "mistral",
+    [ModelTypes.QWEN]: "qwen",
+  };
+
 export type Message = {
-    prompt: string;
-    response: string;
+  prompt: string;
+  response: string;
 };
-  
