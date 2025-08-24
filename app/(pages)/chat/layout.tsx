0369e1b25761900a5799984ff2c22305114139f4
Scrom <formain337@gmail.com>
2025-08-24 22:24:30 +0530

    fix left component rendering again and again

---
+++ app/(pages)/chat/layout.tsx
@@ -0,0 +1,19 @@
+import LeftComponent from "@/app/components/LeftComponent";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="h-screen w-screen grid grid-cols-[15%_85%]">
+          {/* Left loads ONCE and persists */}
+          <LeftComponent />
+
+          {/* Right side is dynamic and changes with routing */}
+          <div className="grid grid-cols-4 grid-rows-[auto_100px] gap-3 p-10 max-h-full h-full overflow-hidden">
+            {children}
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
--- app/components/LeftComponent.tsx
+++ app/components/LeftComponent.tsx
@@ -11,40 +11,29 @@ const LeftComponent = () => {
   useEffect(() => {
     const getChatHistory = async () => {
       const result = await chatHistory(1, 1);
-      
+
       if (result.success) {
-        console.log(result.data?.data.data)
+        console.log(result.data?.data.data);
         appendChat(result.data?.data.data);
       } else {
         console.error("Failed to fetch chat history", result.error);
       }
     };
-  
+
     getChatHistory();
   }, []);
-  
 
   return (
     <div className="w-full grid grid-rows-[150px_auto] p-5 py-10 overflow-hidden">
       <div className="w-full flex flex-col items-center gap-5">
-        <button
-          className="bg-primary rounded-xl w-full h-12 text-sm font-bold hover:cursor-pointer"
-          onClick={() => router.push("/chat/newChat")}
-        >
+        <button className="bg-primary rounded-xl w-full h-12 text-sm font-bold hover:cursor-pointer" onClick={() => router.push("/chat/newChat")}>
           NEW CHAT
         </button>
-        <input
-          type="text"
-          className="bg-primary w-full rounded-xl h-12 text-[13px] focus:outline-0 p-5"
-          placeholder="Search..."
-        />
+        <input type="text" className="bg-primary w-full rounded-xl h-12 text-[13px] focus:outline-0 p-5" placeholder="Search..." />
       </div>
       <div className="w-full h-full flex flex-col items-center gap-5 overflow-auto">
         {chats.map((chat) => (
-          <div
-            key={chat.chatUUID}
-            className="bg-primary rounded-xl w-full min-h-12 text-[12px] flex items-center hover:cursor-pointer p-5"
-          >
+          <div key={chat.chatUUID} className="bg-primary rounded-xl w-full min-h-12 text-[12px] flex items-center hover:cursor-pointer p-5" onClick={()=> router.push(`/chat/${chat.chatUUID}`)}>
             <h3>{chat.chatName}</h3>
           </div>
         ))}
